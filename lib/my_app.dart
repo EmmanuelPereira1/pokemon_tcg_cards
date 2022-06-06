a12714a20e848f1e23dc8977639c4e00d33c952e
EmmanuelPereira1 <97740932+EmmanuelPereira1@users.noreply.github.com>
2022-06-06 14:35:57 -0300

    Revert "container in my app"
    This reverts commit 78438762fa45626d6efbb2c83930f38279dad52b.

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Container(),
+      home: const SizedBox(),
     );
   }
 }
